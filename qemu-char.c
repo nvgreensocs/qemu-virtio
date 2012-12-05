605066b5a415050b743b091c52c8e697900258a9
Marc-André Lureau <marcandre.lureau@gmail.com>
2012-12-05 16:15:34 +0100

    spice-qemu-char: add spiceport chardev
    Add a new spice chardev to allow arbitrary communication between the
    host and the Spice client via the spice server.
    
    Examples:
    
    This allows the Spice client to have a special port for the qemu
    monitor:
    
    ... -chardev spiceport,name=org.qemu.monitor,id=monitorport
        -mon chardev=monitorport
    
    v2:
    - remove support for chardev to chardev linking
    - conditionnaly compile with SPICE_SERVER_VERSION
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- qemu-char.c
+++ qemu-char.c
@@ -2761,6 +2761,9 @@ static const struct {
 #endif
 #ifdef CONFIG_SPICE
     { .name = "spicevmc",     .open = qemu_chr_open_spice },
+#if SPICE_SERVER_VERSION >= 0x000c02
+    { .name = "spiceport",    .open = qemu_chr_open_spice_port },
+#endif
 #endif
 };
 
