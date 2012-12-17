416b7bb8e8246d42c4f87681f679b73a44abe40c
KONRAD Frederic <fred.konrad@greensocs.com>
2012-12-17 15:29:32 +0100

    virtio-scsi : allocate cmd_vqs array separately.
    Allocate/Free the cmd_vqs array separately to have a fixed size device.
    
    Signed-off-by: KONRAD Frederic <fred.konrad@greensocs.com>

--- hw/virtio-scsi.c
+++ hw/virtio-scsi.c
@@ -141,7 +141,7 @@ typedef struct {
     bool events_dropped;
     VirtQueue *ctrl_vq;
     VirtQueue *event_vq;
-    VirtQueue *cmd_vqs[0];
+    VirtQueue **cmd_vqs;
 } VirtIOSCSI;
 
 typedef struct VirtIOSCSIReq {
@@ -700,12 +700,12 @@ VirtIODevice *virtio_scsi_init(DeviceState *dev, VirtIOSCSIConf *proxyconf)
 {
     VirtIOSCSI *s;
     static int virtio_scsi_id;
-    size_t sz;
     int i;
 
-    sz = sizeof(VirtIOSCSI) + proxyconf->num_queues * sizeof(VirtQueue *);
     s = (VirtIOSCSI *)virtio_common_init("virtio-scsi", VIRTIO_ID_SCSI,
-                                         sizeof(VirtIOSCSIConfig), sz);
+                                         sizeof(VirtIOSCSIConfig),
+                                         sizeof(VirtIOSCSI));
+    s->cmd_vqs = g_malloc0(proxyconf->num_queues * sizeof(VirtQueue *));
 
     s->qdev = dev;
     memcpy(&(s->conf), proxyconf, sizeof(struct VirtIOSCSIConf));
