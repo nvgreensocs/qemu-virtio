d5fb7771239bb9d05e68398623ee588812189e92
KONRAD Frederic <fred.konrad@greensocs.com>
2012-12-18 17:14:01 +0100

    virtio : remove the function pointer.
    This remove the function pointer in VirtIODevice, and use only
    VirtioDeviceClass function pointer. It should be applied after all
    the device have been refactored.
    
    Signed-off-by: KONRAD Frederic <fred.konrad@greensocs.com>

--- hw/virtio-scsi.c
+++ hw/virtio-scsi.c
@@ -719,12 +719,6 @@ static int virtio_scsi_device_init(VirtIODevice *vdev)
 
     s->cmd_vqs = g_malloc0(s->conf.num_queues * sizeof(VirtQueue *));
 
-    /* TODO set up vdev function pointers */
-    vdev->get_config = virtio_scsi_get_config;
-    vdev->set_config = virtio_scsi_set_config;
-    vdev->get_features = virtio_scsi_get_features;
-    vdev->reset = virtio_scsi_reset;
-
     s->ctrl_vq = virtio_add_queue(vdev, VIRTIO_SCSI_VQ_SIZE,
                                   virtio_scsi_handle_ctrl);
     s->event_vq = virtio_add_queue(vdev, VIRTIO_SCSI_VQ_SIZE,
