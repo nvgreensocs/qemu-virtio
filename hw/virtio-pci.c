bee0b307291511666c2d9ee2410802999a95b0b0
KONRAD Frederic <fred.konrad@greensocs.com>
2012-12-18 11:45:00 +0100

    virtio-rng : add virtio-rng device.
    Create virtio-rng which extends virtio-device, so it can be connected on
    virtio-bus.
    
    Signed-off-by: KONRAD Frederic <fred.konrad@greensocs.com>

--- hw/virtio-pci.c
+++ hw/virtio-pci.c
@@ -850,13 +850,7 @@ static void virtio_rng_initfn(Object *obj)
 
 static Property virtio_rng_properties[] = {
     DEFINE_VIRTIO_COMMON_FEATURES(VirtIOPCIProxy, host_features),
-    /* Set a default rate limit of 2^47 bytes per minute or roughly 2TB/s.  If
-       you have an entropy source capable of generating more entropy than this
-       and you can pass it through via virtio-rng, then hats off to you.  Until
-       then, this is unlimited for all practical purposes.
-    */
-    DEFINE_PROP_UINT64("max-bytes", VirtIOPCIProxy, rng.max_bytes, INT64_MAX),
-    DEFINE_PROP_UINT32("period", VirtIOPCIProxy, rng.period_ms, 1 << 16),
+    DEFINE_VIRTIO_RNG_PROPERTIES(VirtIOPCIProxy, rng),
     DEFINE_PROP_END_OF_LIST(),
 };
 
@@ -915,6 +909,11 @@ static void virtio_pci_device_plugged(void *opaque)
     case VIRTIO_ID_BALLOON:
         pci_config_set_device_id(proxy->pci_dev.config,
                                  PCI_DEVICE_ID_VIRTIO_BALLOON);
+        pci_config_set_class(proxy->pci_dev.config, PCI_CLASS_OTHERS);
+    break;
+    case VIRTIO_ID_RNG:
+        pci_config_set_device_id(proxy->pci_dev.config,
+                                 PCI_DEVICE_ID_VIRTIO_RNG);
         pci_config_set_class(proxy->pci_dev.config, PCI_CLASS_OTHERS);
     break;
     default:
