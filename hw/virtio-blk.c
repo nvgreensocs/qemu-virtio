5505d3566e047d299af3a6ef3a0cc0f96f2fe29b
KONRAD Frederic <fred.konrad@greensocs.com>
2013-01-04 16:35:33 +0100

    virtio: make virtio device's structures public.
    These structures must be made public to avoid two memory allocations for
    refactored virtio devices.
    
    Signed-off-by: KONRAD Frederic <fred.konrad@greensocs.com>

--- hw/virtio-blk.c
+++ hw/virtio-blk.c
@@ -17,29 +17,10 @@
 #include "hw/block-common.h"
 #include "sysemu/blockdev.h"
 #include "virtio-blk.h"
-#ifdef CONFIG_VIRTIO_BLK_DATA_PLANE
-#include "hw/dataplane/virtio-blk.h"
-#endif
 #include "scsi-defs.h"
 #ifdef __linux__
 # include <scsi/sg.h>
 #endif
-
-typedef struct VirtIOBlock
-{
-    VirtIODevice vdev;
-    BlockDriverState *bs;
-    VirtQueue *vq;
-    void *rq;
-    QEMUBH *bh;
-    BlockConf *conf;
-    VirtIOBlkConf *blk;
-    unsigned short sector_mask;
-    DeviceState *qdev;
-#ifdef CONFIG_VIRTIO_BLK_DATA_PLANE
-    VirtIOBlockDataPlane *dataplane;
-#endif
-} VirtIOBlock;
 
 static VirtIOBlock *to_virtio_blk(VirtIODevice *vdev)
 {
